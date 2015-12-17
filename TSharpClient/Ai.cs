e5e07b8366f26e32be6b7596f3d145df1dc5c97a
Ravi <ravindu.hasantha@gmail.com>
2015-12-17 18:59:51 +0530

    added ai array

---
+++ TSharpClient/Ai.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSharpClient
+{
+    class AI
+    {
+        Array A;
+       
+
+    }
+}
